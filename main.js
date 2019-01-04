3929da9c2e550c6c58e9842530d64c374aa7c348
staniszmatt <matthewpstaniszewski@gmail.com>
2019-01-03 18:50:44 -0800

    Added card flip animation. Added dificulty button with hard level setup with extra cards and images. | Matthew Staniszewski

--- main.js
+++ main.js
@@ -1,43 +1,83 @@
 $(document).ready(startWhenLoaded); 
-var gameCounter = 1;
-var cardFlipCount = 0; 
-var selectedCardArray = [];
+
 var cardArray = 
 ["url('Images/apple.png')", "url('Images/apple.png')", "url('Images/cheekyChocolate.png')", "url('Images/cheekyChocolate.png')",
 "url('Images/coco.png')", "url('Images/coco.png')", "url('Images/cookieCookie.png')", "url('Images/cookieCookie.png')",
 "url('Images/donut.png')", "url('Images/donut.png')", "url('Images/kissingBinky.png')", "url('Images/kissingBinky.png')",
 "url('Images/kisStrawberry.png')", "url('Images/kisStrawberry.png')", "url('Images/libylips.png')", "url('Images/libylips.png')",
 "url('Images/pinapple.png')", "url('Images/pinapple.png')"];
+
+var cardArrayHard = [
+"url('Images/apple.png')", "url('Images/apple.png')", "url('Images/cheekyChocolate.png')", "url('Images/cheekyChocolate.png')",
+"url('Images/coco.png')", "url('Images/coco.png')", "url('Images/cookieCookie.png')", "url('Images/cookieCookie.png')",
+"url('Images/donut.png')", "url('Images/donut.png')", "url('Images/kissingBinky.png')", "url('Images/kissingBinky.png')",
+"url('Images/kisStrawberry.png')", "url('Images/kisStrawberry.png')", "url('Images/libylips.png')", "url('Images/libylips.png')",
+"url('Images/pinapple.png')", "url('Images/pinapple.png')", "url('Images/pancake.png')", "url('Images/pancake.png')",
+"url('Images/cheery.png')", "url('Images/cheery.png')", "url('Images/present.png')", "url('Images/present.png')",
+"url('Images/baloon.png')", "url('Images/baloon.png')", "url('Images/purse.png')", "url('Images/purse.png')"];
+
+var gameDifficultyEasy = true; 
+var gameCounter = 1;
+var cardFlipCount = 0; 
+var selectedCardArray = [];
 var wrongMatchCounter = 0;
 var correctMatchCounter = 0;
 var newSeccessRate = correctMatchCounter/(wrongMatchCounter+correctMatchCounter);
 
 function startWhenLoaded(){
-    randomCardArray();
-    setupCardAttr();
-    $(".card").click(flipCard);
+    cardArray = randomCardArray(cardArray);
+    cardArrayHard = randomCardArray(cardArrayHard);
+    setupCardAttrEasy();
+    clickHandler();
+};
+
+function clickHandler() { 
+    $(".card-layout").on("click", ".card", flipCard);
     $(".reset").click(resetGame);
     $(".new-game").click(newGame);
     $(".pop-up-wrapper").click(toggleGameCompleted);
     $(".about-me").click(aboutMe);
     $(".about-me-text").click(resetAboutMe);
-};
+    $(".difficulty").click(chooseDifficulty)
+    $(".easy").click(easyBoardSetup);
+    $(".hard").click(hardBoardSetup);
+ }
 
-function randomCardArray() {
-    var cardIndex = cardArray.length;
+function randomCardArray(arrayToRandomize) {
+    var cardIndex = arrayToRandomize.length;
     var tempStorage = "";
     var randomIndex = 0; 
     while (0 !== cardIndex) {
         randomIndex = Math.floor(Math.random() * cardIndex);
         cardIndex -= 1; 
-        tempStorage = cardArray[cardIndex];
-        cardArray[cardIndex] = cardArray[randomIndex];
-        cardArray[randomIndex] = tempStorage;
+        tempStorage = arrayToRandomize[cardIndex];
+        arrayToRandomize[cardIndex] = arrayToRandomize[randomIndex];
+        arrayToRandomize[randomIndex] = tempStorage;
     }; 
+    return arrayToRandomize;
 };
 
-function setupCardAttr(){
-    $(".card").addClass("back-of-card no-match");
+function setupCardAttrEasy(){
+    
+    var row1 = $("<div>").addClass("row1");
+    var row2 = $("<div>").addClass("row2");
+    var row3 = $("<div>").addClass("row3");
+    // var row4 = $("<div>").addClass("row4");
+
+    $(".card-layout").append(row1, row2, row3);
+    for (addCardIndex = 0; addCardIndex < 6; addCardIndex++){
+        var card = $("<div>").addClass("card back-of-card no-match");
+        $(".row1").append(card);
+    };
+    for (addCardIndex = 0; addCardIndex < 6; addCardIndex++){
+        var card = $("<div>").addClass("card back-of-card no-match");
+        $(".row2").append(card);
+    };
+    for (addCardIndex = 0; addCardIndex < 6; addCardIndex++){
+        var card = $("<div>").addClass("card back-of-card no-match");
+        $(".row3").append(card);
+    };
+
     var row1Index = 1;
     var row2Index = 1; 
     var row3Index = 1; 
@@ -56,10 +96,57 @@ function setupCardAttr(){
     };
 };
 
+function setupCardAttrHard(){
+    var row1 = $("<div>").addClass("row1");
+    var row2 = $("<div>").addClass("row2");
+    var row3 = $("<div>").addClass("row3");
+    var row4 = $("<div>").addClass("row4");
+
+    $(".card-layout").append(row1, row2, row3, row4);
+    for (addCardIndex = 0; addCardIndex < 7; addCardIndex++){
+        var card = $("<div>").addClass("card back-of-card no-match");
+        $(".row1").append(card);
+    };
+    for (addCardIndex = 0; addCardIndex < 7; addCardIndex++){
+        var card = $("<div>").addClass("card back-of-card no-match");
+        $(".row2").append(card);
+    };
+    for (addCardIndex = 0; addCardIndex < 7; addCardIndex++){
+        var card = $("<div>").addClass("card back-of-card no-match");
+        $(".row3").append(card);
+    };
+    for (addCardIndex = 0; addCardIndex < 7; addCardIndex++){
+        var card = $("<div>").addClass("card back-of-card no-match");
+        $(".row4").append(card);
+    };
+
+    var row1Index = 1;
+    var row2Index = 1; 
+    var row3Index = 1; 
+    var row4Index = 1; 
+    for (var cardIdIndex = 0; cardIdIndex < cardArrayHard.length; cardIdIndex++){
+        var cardImage = cardArrayHard[cardIdIndex];
+        if (cardIdIndex < 7){
+            $(".row1 .card:nth-child("+row1Index+")").attr("id", cardImage);
+            row1Index++;
+        } else if (cardIdIndex > 6 && cardIdIndex < 14) {
+            $(".row2 .card:nth-child("+row2Index+")").attr("id", cardImage);
+            row2Index++;
+        }else if (cardIdIndex > 13 && cardIdIndex < 21) {
+            $(".row3 .card:nth-child("+row3Index+")").attr("id", cardImage);
+            row3Index++;
+        } else {
+            $(".row4 .card:nth-child("+row4Index+")").attr("id", cardImage);
+            row4Index++;
+        };
+    };
+};
+
 function flipCard() { 
     flipCardSound();
     $(this).removeClass("back-of-card");
     $(this).addClass("front-of-card");
+    // $(this).addClass("flip-card");
     var imageID = $(this).attr("id");
     $(this).css("background-image", imageID);
     cardFlipCount++
@@ -67,6 +154,8 @@ function flipCard() {
     cardMatchCheck(selectedCardArray);
     gameCompletedSound();
  };
+
+
 
 function cardMatchCheck() { 
     var arrayLength = selectedCardArray.length;
@@ -131,11 +220,25 @@ function newGame(){
 };
 
 function cardReset() { 
-    $(".card").removeAttr("style");
-    $(".card").removeClass("front-of-card");
-    randomCardArray();
-    setupCardAttr();
+    cardArray = randomCardArray(cardArray);
+    cardArrayHard = randomCardArray(cardArrayHard);
     clearCardMatchCheck();
+
+    $(".card").remove();
+    $(".row1").remove();
+    $(".row2").remove();
+    $(".row3").remove();
+    $(".row4").remove();
+    if (gameDifficultyEasy===true){
+        setupCardAttrEasy();
+    } else {
+        setupCardAttrHard();
+    }
+    
+ };
+ 
+ function chooseDifficulty(){
+    $(".drop-down-menu").toggle("display");
  };
 
 function verifyGameCompleted(){
@@ -173,10 +276,31 @@ function aboutMe() {
     $(".top-container").toggle("display"); 
     $(".ok-read-me").toggle("display");
     $(".about-me").toggle("display");
+  };
+  
+  function easyBoardSetup(){
+    $(".drop-down-menu").toggle("display");
+    gameDifficultyEasy = true;
+    resetGame();
+  };
 
-  }
+  function hardBoardSetup(){
+    $(".drop-down-menu").toggle("display");
+    cardArrayHard = randomCardArray(cardArrayHard);
+    clearCardMatchCheck();
 
-function flipCardSound() { 
+    $(".card").remove();
+    $(".row1").remove();
+    $(".row2").remove();
+    $(".row3").remove();
+    $(".row4").remove();
+    setupCardAttrHard();
+    gameDifficultyEasy = false; 
+  };
+  
+  
+  
+  function flipCardSound() { 
     var cardFlipSound = new Audio("sound/Card-flip-sound-effect.mp3");
     cardFlipSound.play();
  };
